d53b7bebc68e2c5b592df52cfe866723e7d6dbc9
frederik.steffen <frederik.steffen@gmail.com>
2020-09-10 22:59:59 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main id="app">
+    <el-form inline @submit.native.prevent>
+      <el-form-item>
+        <el-input
+          placeholder="Enter your search..."
+          v-model="query"
+          clearable
+          @change="search"
+        />
+      </el-form-item>
+    </el-form>
+    <div class="tinder" v-if="gifs.length > 0">
+      <img :src="gifs[index].media[0].gif.url" />
+      <div class="actions">
+        <el-button @click="next">Next</el-button>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      query: "",
+      gifs: [],
+      index: 0
+    };
+  },
+  methods: {
+    async search() {
+      const key = process.env.VUE_APP_TENOR_API_KEY;
+      const res = await fetch(
+        `https://api.tenor.com/v1/search?q=${this.query}&key=${key}`
+      );
+      const data = await res.json();
+      this.gifs = data.results;
+    },
+    next() {
+      if (this.index < this.gifs.length) {
+        this.index++;
+      }
+    }
   }
 };
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+  display: grid;
+  background-color: #eaeaea;
+}
+
+.el-form .el-input[type="text"] {
+  width: 500px;
+}
+
+.tinder img {
+  margin: auto;
+}
+
+.tinder .actions {
+  margin: 10px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
